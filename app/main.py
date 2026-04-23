05d06bdb33a0b73864669a084715c5db3550d72a
vinya14-rai <vinya14@gmail.com>
2026-04-23 16:10:08 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from app.db import get_results
+from app.analysis import get_top_stocks
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Running"}
+    return {"message": "Stock Analyzer Running"}
 
 @app.get("/top-stocks")
 def top_stocks():
-    return get_results()
+    return get_top_stocks()
